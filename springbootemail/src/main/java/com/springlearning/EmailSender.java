66f4db39e9cba2b86ce9c3fc81de45056a2574b0
rebecca.varadi <rebecca.varadi@vistair.com>
2022-03-31 15:27:32 +0100

    mail update

--- springbootemail/src/main/java/com/springlearning/EmailSender.java
+++ springbootemail/src/main/java/com/springlearning/EmailSender.java
@@ -3,13 +3,15 @@ package com.springlearning;
 import com.springlearning.model.EMail;
 import com.springlearning.services.MailSenderSpring;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class EmailSender {
     @Autowired
     MailSenderSpring mailSenderSpring;
@@ -17,14 +19,27 @@ public class EmailSender {
     @RequestMapping("/sendmail")
     public void sendMessage() {
         EMail email = new EMail();
-        email.setTo("krishankantsinghal@gmail.com");
-        email.setFrom("happinessproject@vistair.com");
+        email.setTo("projecthapiness5@gmail.com");
+        email.setFrom("projecthapiness5@gmail.com");
         email.setSubject("Happiness Project");
         email.setContent("Sending mail");
         Map<String, Object> model = new HashMap<>();
-        model.put("firstName", "FirstName");
-        model.put("lastName", "LastName");
+        model.put("firstName", "Happiness");
+        model.put("lastName", "Prj");
         mailSenderSpring.sendEmailWithTemplate(email);
+    }
+
+    @RequestMapping("/sendmail-attachment")
+    public void sendMessageWithAttachment() throws IOException, MessagingException {
+        EMail email = new EMail();
+        email.setTo("projecthapiness5@gmail.com");
+        email.setFrom("projecthapiness5@gmail.com");
+        email.setSubject("Happiness Project");
+        email.setContent("Sending mail");
+        Map<String, Object> model = new HashMap<>();
+        model.put("firstName", "Happiness");
+        model.put("lastName", "Prj");
+        mailSenderSpring.sendEmailWithAttachment(email);
     }
 
 }
